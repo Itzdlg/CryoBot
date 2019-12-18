2952f8656d02a22661d916b62239f8e917652689
Itzdlg <dontyoutrybrah@gmail.com>
2019-12-17 22:08:07 -0500

    Refurbished code in CommandListener

--- src/main/java/me/schooltests/cryobot/modules/core/listeners/CommandListener.java
+++ src/main/java/me/schooltests/cryobot/modules/core/listeners/CommandListener.java
@@ -2,12 +2,14 @@ package me.schooltests.cryobot.modules.core.listeners;
 
 import me.schooltests.cryobot.internal.BaseCommand;
 import me.schooltests.cryobot.modules.core.CoreModule;
+import me.schooltests.cryobot.modules.core.util.CommandUtil;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 
 public class CommandListener extends ListenerAdapter {
     private CoreModule module;
+    private final CommandUtil commandUtil = getModule().getCommandUtil();
     public CommandListener(CoreModule module) {
         this.module = module;
     }
@@ -18,14 +20,14 @@ public class CommandListener extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        if(!event.getAuthor().isBot()) {
-            if (event.getMessage().getContentRaw().startsWith(getModule().getCommandUtil().getCommandPrefix())) {
-                String[] args = event.getMessage().getContentRaw().split(" ");
-                String commandPart = args[0].replaceFirst(getModule().getCommandUtil().getCommandPrefix(), "");
-                for (BaseCommand command : getModule().getRegistryService().getCommands()) {
-                    if (command.getIdentifier().equalsIgnoreCase(commandPart) || command.getAliases().contains(commandPart.toLowerCase())) {
-                        getModule().getCommandUtil().handleCommandEvent(event, command, getModule().getCommandUtil().removeElementFromArray(args, 0));
-                    }
+        if (event.getAuthor().isBot()) return;
+        String rawContent = event.getMessage().getContentRaw();
+        if (rawContent.startsWith(commandUtil.getCommandPrefix())) {
+            String[] args = rawContent.split(" ");
+            String commandPart = args[0].replaceFirst(commandUtil.getCommandPrefix(), "");
+            for (BaseCommand command : getModule().getRegistryService().getCommands()) {
+                if (command.getIdentifier().equalsIgnoreCase(commandPart) || command.getAliases().contains(commandPart.toLowerCase())) {
+                    commandUtil.handleCommandEvent(event, command, commandUtil.removeElementFromArray(args, 0));
                 }
             }
         }
